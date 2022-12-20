f2cbd527ad82d75ae076c4551928d9d836fd0978
Kiss, Tibor <tibor.kiss01@sap.com>
2022-12-20 16:43:10 +0100

    Extarnal call

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HealthCheck struct {
+	Component string `json:"component" binding:"required"`
+	Status    string `json:"status"`
+}
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/healthCheck", func(c *gin.Context) {
-		c.JSON(200, gin.H{"status": "OK"})
+	router.POST("/healthCheck", func(c *gin.Context) {
+		var h HealthCheck
+		if err := c.ShouldBindJSON(&h); err != nil {
+			h.Status = "error"
+			c.JSON(400, h)
+
+			return
+		}
+		log.Printf("I am OK: %v", h.Component)
+		h.Status = "OK"
+		c.JSON(200, h)
 	})
 
 	router.Run(":4000")
